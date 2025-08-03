5d30021af45287c416822634b77d37cb284ce6cd
Andika Dinata <a.introvrt@gmail.com>
2025-08-03 19:52:33 +0700

    Initial commit.

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import js from "@eslint/js"
+import globals from "globals"
+import css from "@eslint/css"
+import { defineConfig } from "eslint/config"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  eslintConfigPrettier,
+])
